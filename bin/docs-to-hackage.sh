34b73106baff798dac1040d8bd5bcbe5104bbb9f
Fuzz Leonard <cyborg@bionicfuzz.com>
2025-03-31 12:25:47 -0700

    Update documentation upload script to mitigate curl issue

---
+++ bin/docs-to-hackage.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+# Must force HTTP 1.1 to get around networking issues
+curl --http1.1 -X PUT --data-binary '@dist-newstyle/clod-0.1.1-docs.tar.gz' -H 'Content-Type: application/x-tar' -H 'Content-Encoding: gzip' --user fuzz https://hackage.haskell.org/package/clod-0.1.1/docs
